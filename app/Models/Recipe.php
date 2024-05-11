7f9a801ccef42340633a9dab83091b9707e83895
NujanSitaula <nujan.sitaula@outlook.com>
2024-05-11 19:25:12 +0545

    Integrated Algolia Search in recipe

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    use HasFactory;
+    use Searchable;
 }
