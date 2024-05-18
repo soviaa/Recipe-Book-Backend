8588a96357ac7732f4baa80b82be4fc7f4b4a200
Sovia <59617775+soviaa@users.noreply.github.com>
2024-05-18 22:37:20 +0545

    Seeders created
    Co-authored-by: NujanSitaula <nujan.sitaula@outlook.com>

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +14,17 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->insert([
+            'name' => 'Indian',
+            'slug' => '/indian',
+            'description' => 'This is  indian.',
+            'image' => '/sample.jpg',
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Nepali',
+            'slug' => '/nepali',
+            'description' => 'This is nepali.',
+            'image' => '/sample.jpg',
+        ]);
     }
 }
