af5cb9331cb4db5024e158e3fec5673f6d369c5c
NujanSitaula <nujan.sitaula@outlook.com>
2024-05-18 21:29:47 +0545

    Comment Feature Fully Added
    Co-authored-by: Soviaa <soviamdr@gmail.com>

--- app/Http/Controllers/User/CommentsController.php
+++ app/Http/Controllers/User/CommentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
+use App\Models\CommentReply;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
@@ -20,7 +21,7 @@ class CommentsController extends Controller
 
     public function commentSingle($id)
     {
-        $comment = Comment::with('user', 'replies.user')->where('id', $id)->get()->toArray();
+        $comment = Comment::with('user', 'replies.user')->where('recipe_id', $id)->get()->toArray();
         if($comment){
             return response()->json([
                 'status' => 'success',
@@ -35,5 +36,48 @@ class CommentsController extends Controller
                 'data' => null
             ], 404);
         }
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'comment' => 'required',
+            'recipe_id' => 'required'
+        ]);
+
+        $comment = new Comment();
+        $comment->comment = $request->comment;
+        $comment->user_id = 2;
+        $comment->recipe_id = $request->recipe_id;
+        $comment->save();
+
+        $comment = Comment::with('user')->where('id', $comment->id)->get()->toArray();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Comment added successfully',
+            'data' => $comment
+        ], 201);
+    }
+
+    public function replyStore(Request $request, $id)
+    {
+        $request->validate([
+            'reply' => 'required',
+        ]);
+
+        $reply = new CommentReply();
+        $reply->reply = $request->reply;
+        $reply->user_id = 1;
+        $reply->comment_id = $id;
+        $reply->save();
+
+        $reply = CommentReply::with('user')->where('id', $reply->id)->get()->toArray();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Reply added successfully',
+            'data' => $reply
+        ], 201);
     }
 }
