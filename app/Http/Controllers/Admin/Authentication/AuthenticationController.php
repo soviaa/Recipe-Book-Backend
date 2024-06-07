9c65ab04841515a3a41c50dcdff49b8afb7debb0
NujanSitaula <nujan.sitaula@outlook.com>
2024-06-07 09:29:48 +0545

    Feature: Push notification queue added, real data passed in the notification.
    Co-authored-by: Soviaa <soviamdr@gmail.com>

--- app/Http/Controllers/Admin/Authentication/AuthenticationController.php
+++ app/Http/Controllers/Admin/Authentication/AuthenticationController.php
@@ -2,38 +2,36 @@
 
 namespace App\Http\Controllers\Admin\Authentication;
 
-
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Admin;
 use Hash;
+use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
     public function login(Request $request)
     {
-        try{
+        try {
             $credentials = $request->only('email', 'password');
             $user = Admin::where('email', $credentials['email'])->first();
-            if(! Hash::check($credentials['password'], $user->password)){
+            if (! Hash::check($credentials['password'], $user->password)) {
                 return response()->json([
                     'message' => 'Invalid credentials'], 401);
             }
             $token = $user->createToken('token-name')->plainTextToken;
+
             return response()->json([
                 'access_token' => $token,
                 'message' => 'Login successful'], 200);
 
-        }
-        catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             echo $e->getMessage();
         }
     }
+
     public function index()
     {
         return response()->json([
             'message' => 'Welcome to the admin panel'], 200);
     }
-
 }
