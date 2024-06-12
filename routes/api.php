ea8ce2eaf0a80363b9dd76bb8463486d97b3ddd5
NujanSitaula <nujan.sitaula@outlook.com>
2024-06-12 16:11:44 +0545

    Feature: Push notification queue added, real data passed in the notification.
    Co-authored-by: Soviaa <soviamdr@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FollowController;
 use App\Http\Controllers\RecipeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -19,7 +19,6 @@ Route::post('/user/setting', 'App\Http\Controllers\User\Authentication\Authentic
 
 Route::get('/user/{username}', 'App\Http\Controllers\User\UserController@getUser');
 
-
 Route::post('/user/login', 'App\Http\Controllers\User\Authentication\AuthenticationController@login');
 Route::get('/user/index', 'App\Http\Controllers\User\Authentication\AuthenticationController@index')->middleware('auth:sanctum');
 
@@ -29,18 +28,15 @@ Route::post('/recipe', 'App\Http\Controllers\RecipeController@addRecipe')->middl
 
 Route::get('/ingredient', 'App\Http\Controllers\IngredientController@index');
 
-
 Route::get('/category', 'App\Http\Controllers\CategoryController@index');
 Route::get('/recipe/{id}', 'App\Http\Controllers\RecipeController@recipeSingle');
 
 Route::get('/comment', 'App\Http\Controllers\User\CommentsController@index');
 Route::get('/comment/{id}', 'App\Http\Controllers\User\CommentsController@commentSingle');
 
-
 Route::post('/user/tfa/generate', 'App\Http\Controllers\Auth\TfaController@twoFactorGenerate')->middleware('auth:sanctum');
 Route::post('/user/tfa/verify', 'App\Http\Controllers\Auth\TfaController@twoFactorVerify')->middleware('auth:sanctum');
 Route::post('/user/tfa/disable', 'App\Http\Controllers\Auth\TfaController@twoFactorDisable')->middleware('auth:sanctum');
-
 
 Route::middleware('auth:sanctum')->group(function () {
 
@@ -51,10 +47,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/followees/{userId}', [FollowController::class, 'followees'])->name('followees');
     Route::get('/follow/{userId}/status', [FollowController::class, 'checkFollowStatus'])->name('follows');
 
-
     Route::post('/comment/{id}/reply', 'App\Http\Controllers\User\CommentsController@replyStore');
     Route::post('/comment', 'App\Http\Controllers\User\CommentsController@store');
 
 });
 
-
+Route::get('/ml', 'App\Http\Controllers\MachineLearning\GeminiTestController@index');
+Route::get('/ml/image', 'App\Http\Controllers\MachineLearning\GeminiTestController@imageIdentifier');
